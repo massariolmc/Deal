9fef00da93d4eabafa4eeed4280e94cda6fccdb6
Maximiano Massariol Cardoso <massariolmc@gmail.com>
2021-07-21 12:05:11 -0400

    Fix datatables Tax Invoice

--- tax_invoice/static/tax_invoice/default/js/jquery_mask.js
+++ tax_invoice/static/tax_invoice/default/js/jquery_mask.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){ 
     'use strict';   
     //$('.cep').mask('00000-000');    
-    //$('.cnpj').mask('00.000.000/0000-00');  
-    $('.money').mask('000.000.000.000.000,00', {reverse: true}); 
-    $('.ref').mask("00/0000" );     
+    $('.cnpj').mask('00.000.000/0000-00');  
+    //$('.money').mask('000.000.000.000.000,00', {reverse: true}); 
+    //$('.ref').mask("00/0000" );     
 
 });
 
